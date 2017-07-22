1964d70eb20869e9d37e0eb47c239e6717dcd951
T L <tslimkemann42@gmail.com>
2017-07-22 11:12:34 -0700

    Fixed optionsFragment layout

--- app/src/main/java/io/aearnus/stocksbib/OptionsFragment.java
+++ app/src/main/java/io/aearnus/stocksbib/OptionsFragment.java
@@ -14,6 +14,6 @@ import android.view.ViewGroup;
 public class OptionsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_stock_currency, container, false);
+        return inflater.inflate(R.layout.fragment_stock_options, container, false);
     }
 }
